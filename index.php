4f3791c93122f916864129b23be743412f075828
Santiago Lopez <slopez@rabeda.gal>
2016-03-14 19:08:23 +0100

    comiteo el index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>
+	</title>
+</head>
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea est ex minima debitis deleniti nam quasi placeat eveniet, voluptate impedit assumenda blanditiis velit qui, aspernatur dolores. Est dignissimos adipisci maxime.</p>
+</body>
+</html>
